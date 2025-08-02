70f9402700fdee2513e61ec8fb5d2dda2f04427d
upilbalmon <balmonupil@gmail.com>
2025-08-02 21:19:51 +0700

    Update teleport.lua

--- teleport.lua
+++ teleport.lua
@@ -13,7 +13,7 @@ local remoteEvent = ReplicatedStorage:WaitForChild("Msg"):WaitForChild("RemoteEv
 local BUTTON_WIDTH = 80
 local BUTTON_HEIGHT = 25
 local BUTTON_SPACING = 10
-local BASE_POSITION = CFrame.new(-16.66, 3.39, -4953.99)  -- Tetap sebagai default posisi base
+local BASE_POSITION = CFrame.new(14.25, -5.62, -116.60)  -- Tetap sebagai default posisi base
 local HIDE_POSITION = CFrame.new(10.84, 16395.57, -250.11)
 local FLY_SPEED = 50
 
@@ -316,5 +316,6 @@ player.CharacterRemoving:Connect(function()
         toggleFly() -- Turn off fly when character is removed
     end
 end)
+
 
 
